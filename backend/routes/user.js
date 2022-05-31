f99ed86ab1dea7c2fdf9b3ec60a39cc59c39b312
Alexander Nagy <nagyal3x@gmail.com>
2022-05-31 11:43:36 +0200

    auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,61 @@
-const router = require('express').Router()
+const User = require("../model/user");
+const httpModule = require("../utils/http");
+const http = httpModule();
+const router = require("express").Router();
 
-router.post('/api/login', async(req, res) => {
-    // recieve google code -> get google token -> get userID
-    // googleId exists ? send jwt token : create user 
-    res.sendStatus(418)
-})
+const config = {
+  google: {
+    client_id: "",
+    client_secret: "",
+    redirect_uri: "",
+    token_endpoint: "",
+    grant_type: "",
+  },
+  facebook: {
+    client_id: "", // or appId
+    client_secret: "", // or appSecret
+    redirect_uri: "",
+    token_endpoint: "",
+    grant_type: "",
+  },
+};
 
-router.get('/', async(req, res) => {
-    // try
-    res.status(418).json("test👋")
-})
+router.post("/api/login", async (req, res) => {
+  const payload = req.body;
+  if (!payload) return res.sendStatus(400); // need a req body
 
-module.exports = router
+  const code = payload.code;
+  const provider = payload.provider;
+
+  if (!code || !provider) return res.sendStatus(400); // not enough data - should have code & provider
+  if (!Object.keys(config).includes(provider)) return res.sendStatus(400); // if config{ doestn't have provider } 
+
+  const response = await http.post(config[provider].token_endpoint, {
+    code: code,
+    client_id: config[provider].client_id,
+    client_secret: config[provider].client_secret,
+    redirect_uri: config[provider].redirect_uri,
+    grant_type: "authorization_code",
+  });
+
+  if (!response) return res.sendStatus(500);
+  if (response.status != 200) return res.sendStatus(401);
+
+  const decoded = jwt.decode(response.data.id_token)
+
+  if(!decoded) return res.sendStatus(500)
+
+  const key = 'providers.' + provider
+  const user = await User.find({[key]: decoded.sub})  // hmm
+
+});
+
+// recieve google code -> get google token -> get userID
+// googleId exists ? send jwt token : create user
+
+router.get("/", async (req, res) => {
+  // try
+  res.status(418).json("test👋");
+});
+
+module.exports = router;
